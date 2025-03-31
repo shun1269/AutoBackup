66f1d94cc63fea616e5eb5d388f0d6728ac73041
samidare <shun2048@icloud.com>
2025-04-01 02:23:24 +0900

    feat

--- backup.sh
+++ backup.sh
@@ -1,1 +1,9 @@
-SOUCE_FILE  = ""
+SOUCE_FILE  = "/etc/minecraft_server/forge_1.20.1/world"
+BACKUP_DIR = "/etc/minecraft_server/backup_forge_1.20.1"
+DATE=$(date +"%Y+%m+%d")
+BACKUP_NAME="backup_${DATE}"
+BACKUP_PATH="${BACKUP_DIR}/${BACKUP_DIR}"
+
+mkdir -p "$BACKUP_PATH"
+cp "$SOUCE_FILE" "$BACKUP_PATH"
+echo "Backup completed!: $BACKUP_PATH"
