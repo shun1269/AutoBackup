0f5a68a4f8428282a90abf66bb40d505f4210feb
samidare <shun2048@icloud.com>
2025-04-01 03:16:53 +0900

    feat

--- backup.sh
+++ backup.sh
@@ -1,3 +1,3 @@
-mkdir -p /etc/minecraft_server/backup_forge_1.20.1/backups/$(date + "%Y%m%d-%H")
-cp -r /etc/minecraft_server/forge_1.20.1/world /etc/minecraft_server/backup_forge_1.20.1/backups/$(date + "%Y%m%d-%H")
-echo "Backup completed!: $(date + "%Y%m%d-%H")"
+mkdir -p /etc/minecraft_server/backup_forge_1.20.1/backups/$(date +"%Y%m%d-%H")
+cp -r /etc/minecraft_server/forge_1.20.1/world /etc/minecraft_server/backup_forge_1.20.1/backups/$(date +"%Y%m%d-%H")
+echo "Backup completed!" ; date +"%Y%m%d-%H"
